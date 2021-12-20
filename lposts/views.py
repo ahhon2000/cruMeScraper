a899194701cd0beeb4beaad11560a948faa1d5f3
ahhon <polygon2000x@gmail.com>
2021-12-21 00:54:46 +0400

    added Bootstrap

--- lposts/views.py
+++ lposts/views.py
@@ -5,11 +5,13 @@ from django.views.generic import TemplateView
 
 from cruMeScraper.settings import LPOSTS
 from lposts.models import Post
+from cruMeScraper.jinja2 import staticLinkMaker
 
 class LatestPostsView(TemplateView):
     template_name = 'lposts/latest_posts.html'
 
     def get_context_data(self, **kwarg):
+        app_name = 'lposts'
         ctx = super().get_context_data(**kwarg)
         N = LPOSTS['N_POSTS']
         ts = OrderedDict(map(
@@ -32,7 +34,11 @@ class LatestPostsView(TemplateView):
         ))
         ctx.update({
             'tables': ts,
-            'fmtDate': lambda d: d.strftime("%Y-%m-%d %H:%M:%S")
+            'fmtDate': lambda d: d.strftime("%Y-%m-%d %H:%M:%S"),
+            'appStatic': staticLinkMaker(f'{app_name}/'),
+            'fmtExcerpt': lambda e: \
+                    e + '...' if e and not e[-1] in "!?." else e,
+            'enumerate': enumerate,
         })
 
         return ctx
